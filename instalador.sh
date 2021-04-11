5e6eed5be82425ba5320efeba6a31cc76f1f1dff
Russo <81944631+Russo-ofc@users.noreply.github.com>
2021-04-10 23:11:35 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,4 +1,4 @@
-        #!/bin/bash
+#!/bin/bash
 set -e
 
 ##################################################################################
@@ -11,51 +11,53 @@ set -e
 
 
 
-        output(){
-            echo -e '\e[36m'$1'\e[0m';
-        }
+output(){
+echo -e '\e[36m'$1'\e[0m';
+}
         
-        warn(){
-            echo -e '\e[31m'$1'\e[0m';
-        }
+warn(){
+echo -e '\e[31m'$1'\e[0m';
+}
         
-        PANEL=v0.0.1
+PANEL=v0.0.1
         
+ preflight(){
+output "Script V-0.0.1"
+output "Copyright © 2021."
+}
         
-        preflight(){
-            output "Script V-0.0.1"
-            output "Copyright © 2021."
-            output ""
-        }
+install_options(){
+output "Selecionar Oque Instalar Adrian:"
+output "[1] Install the panel ${PANEL}."
+read choice
+case $choice in
+1 ) installoption=1
+output "You have selected ${PANEL} panel installation only."
+;;
+* ) output "You did not enter a valid selection."
+install_options
+esac
+}
+update() {
+echo "* Update Do SISTEMA"
+apt-get update
+echo "* UPGRADE DO SISTEMA"
+apt -y upgrade
         
-        install_options(){
-            output "Selecionar Oque Instalar Adrian:"
-            output "[1] Install the panel ${PANEL}."
+echo "FINALIZADO"
+}
+
+dependencias() {
+echo "* Baixando Todas As Dependencias..."
+apt-get install -y software-properties-common apache2 mysql-server php libapache2-mod-php php-mysql php-gd php-cli php-common php-mbstring php-ldap php-odbc php-pear php-xml php-xmlrpc php-bcmath php-pdo default-jdk git zip unzip python-certbot-apache vimapt-get install -y software-properties-common apache2 mysql-server php libapache2-mod-php php-mysql php-gd php-cli php-common php-mbstring php-ldap php-odbc php-pear php-xml php-xmlrpc php-bcmath php-pdo default-jdk git zip unzip python-certbot-apache vim
         
-            read choice
-            case $choice in
-                1 ) installoption=1
-                    output "You have selected ${PANEL} panel installation only."
-                    ;;
-                * ) output "You did not enter a valid selection."
-                    install_options
-            esac
-        }
-        
-        update() {
-            echo "* Update Do SISTEMA"
-            apt-get update
-        
-            echo "* UPGRADE DO SISTEMA"
-            apt -y upgrade
-        
-            echo "FINALIZADO"
-        }
-        
-        
-        #Execution
-        install_options
-        case $installoption in 
-                1)   update
-                     ;;
-        esac
+echo "* OK"
+}
+
+#Execution
+install_options
+case $installoption in 
+1)   update
+dependencias
+;;
+esac
