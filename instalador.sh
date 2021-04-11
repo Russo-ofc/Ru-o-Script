06b3369dcdb7dc8fa73670968def6a30c1d563a4
Russo <81944631+Russo-ofc@users.noreply.github.com>
2021-04-11 15:21:23 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -34,24 +34,26 @@ case $choice in
 1 ) installoption=1
 output "You have selected ${PANEL} panel installation only."
 ;;
-* ) output "You did not enter a valid selection."
+* ) output "Selecione Uma Das Opções Corretas."
 install_options
 esac
 }
-update() {
+update_geral() {
 echo "* Update Do SISTEMA"
 apt-get update
+echo "* UPDATE FINALIZADO"
+echo "*"
 echo "* UPGRADE DO SISTEMA"
 apt -y upgrade
-        
-echo "FINALIZADO"
+echo "* UPGRADE FINALIZADO"
+echo "*"    
 }
 
-dependencias() {
+dependencias_multicraft() {
 echo "* Baixando Dependencias PhP..."
 apt install -y php libapache2-mod-php php-mysql php-gd php-cli php-common php-mbstring php-ldap php-odbc php-pear php-xml php-xmlrpc php-bcmath php-pdo
 echo "* Baixando Todas As Dependencias..."
-apt install -y software-properties-common apache2  mysql-server default-jdk git zip unzip python-certbot-apache vim
+apt install -y software-properties-common apache2  mysql-server default-jdk git zip unzip python-certbot-apache
         
 echo "* OK"
 }
@@ -59,7 +61,7 @@ echo "* OK"
 #Execution
 install_options
 case $installoption in 
-1)   update
-dependencias
+1)   update_geral
+dependencias_multicraft
 ;;
 esac
